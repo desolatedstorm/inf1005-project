63822c6d25a53f70b8c82107f4de9c17332a35ad
loghogjog <tantris.tan345@gmail.com>
2025-11-26 23:37:43 +0800

    blocking exposed endpoints

--- src/cancel_booking.php
+++ src/cancel_booking.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once __DIR__ . "/inc/login_functions.php";
+require_once __DIR__ . "/inc/functions.php";
 require_once __DIR__ . "/vendor/autoload.php";
 require_once __DIR__ . "/api/api_send_email.php";
 
